a51271c644f5d3ac415a4d5d1c7aa929ab963016
sbohlen <sbohlen@gmail.com>
2019-04-27 12:14:30 -0400

    failing test for GeoLocationCoder

---
+++ test/FriendlyFoodFinder.Core.Test/WhenGeocodingAddress.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Constraints;
+
+namespace FriendlyFoodFinder.Core.Test
+{
+    [TestFixture]
+    public class WhenGeocodingAddress
+    {
+        private const string TEST_ADDRESS_STRING = "11 Times Square, New York, New York";
+        private GeoLocation expectedGeoLocation;
+
+        [SetUp]
+        public void SetUp()
+        {
+            expectedGeoLocation = new GeoLocation(1d,2d);
+        }
+ 
+
+        [Test]
+        public void CanGeocodeValidAddress()
+        {
+            var geoCoder = new GeoLocationCoder();
+            var geoLocation = geoCoder.GeoCode(TEST_ADDRESS_STRING);
+
+            Assert.That(geoLocation, Is.EqualTo(expectedGeoLocation));
+        }
+    }
+
+    public class GeoLocationCoder
+    {
+        public GeoLocation GeoCode(string address)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class GeoLocation
+    {
+        public GeoLocation(double longitude, double latitude)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
